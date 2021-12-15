4dde6c8e8f6f8dee67c1b0e9f0f2521a43ad7fe6
unknown <mrafaelperez94@hotmail.com>
2021-12-15 20:16:21 -0300

    glops v0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/NavBar';
+import Section from './components/Section1';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App (){
+    return (
+        <>
+        <Nav/>
+        <Section/>
+        <Footer/>
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Section1.js
@@ -0,0 +1,13 @@
+function Section (){
+
+    return (
+        <div>
+            <img src="https://i.ibb.co/RhGsMZ6/background.png" alt="" />
+        </div>
+
+    )
+
+
+}
+
+export default Section
